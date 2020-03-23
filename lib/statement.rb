94f707bc33928ddebd8576bce25146e469c1573a
Josh Blumberg <jlblumberg@gmail.com>
2020-03-23 14:33:18 +0000

    Add statement rows to statement class

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,7 +1,15 @@
 class Statement
+  
+  def initialize
+    @statement_rows = []
+  end
 
   def print_statement
-    ""
+    @statement_rows.join('\n')
+  end
+
+  def add_header
+    @statement_rows.push("date || credit || debit || balance")
   end
 
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,9 +1,18 @@
 require 'statement'
 
 describe Statement do
+
   describe '#print_statement' do
     it 'prints the statement' do
       expect(subject.print_statement).to eq("")
     end
   end
+
+  describe '#add_header' do
+    it 'Adds the statement header to the statement array'do
+      subject.add_header
+      expect(subject.print_statement).to eq("date || credit || debit || balance")
+    end
+  end
+
 end
